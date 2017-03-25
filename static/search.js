56bad15d426f84731c4726882739a4b17d2a03ba
Abdullahi Diriye <aidiriye001@gmail.com>
2017-03-25 16:25:18 -0700

    Improved user interface , increased search capabilities
    - Added css to improve aesthics
    - Changed the format of html label elements to match standard HTML
    guidelines.
    
    - Added loading animation that runs until results are ready to be
    displayed.
    - Igt list items that are selected are now highlighted.
    - Igts list items that are hovered over by the mouse become bold now.
    
    - Added the option to search igt tiers: glosses, morphemes, and pos in
    addition to the previous default tier of words. A client chooses which
    tier to search using a drop down option box.

--- static/search.js
+++ static/search.js
@@ -2,8 +2,9 @@
 	'use strict';
 
 	// attaches a method to the calculat ebutton
+	// adds options to the corpus drop down menu
 	window.onload = function () {
-		d3.xhr("http://127.0.0.1:5000/v1/corpora")
+		d3.xhr("http://odin.xigt.org/v1/corpora")
         .mimeType("application/json")
         .response(function(xhr) { return JSON.parse(xhr.responseText); })
         .get(function(error, d) {
@@ -12,25 +13,40 @@
               .selectAll("option")
               .data(d.corpora)
               .enter().append("option")
+              
                 .text(function(d) {
                     return d.name + " (" + d.igt_count + ") ";
                 })
+   
                 .attr("value", function(d){return d.id;});
+
       
         });
         
 	};
 
 
+
 	
-}());
+	
+})();
+
+var currListItem = null;
 
 function lookFor(){
 		d3.select("#num").selectAll("p").remove();
 		d3.select("#igt_id").selectAll("li").remove();
+		$("#igt").html("");
+		$("#loadingicon").show();
+
+		$("#num").hide();
+		$("#igt_id").hide();
+		
+
 		var name = document.forms.searchForm.corpus.value;
 		var word = document.forms.searchForm.words.value;
-		d3.xhr("http://127.0.0.1:5000/v1/corpora/" + name + "/igts?match=" + encodeURIComponent("tier[@type=\"words\"]/item[value()=\"" + word + "\"]"))
+		d3.xhr("http://odin.xigt.org/v1/corpora/" + name + "/igts?match=" + 
+					encodeURIComponent("tier[@type=\""+  $("#tiertype").val()  +"\"]/item[value()=\"" + word + "\"]"))
 			.mimeType("application/json")
 			.response(function(xhr) { return JSON.parse(xhr.responseText); })
 			.get(function(error, d) {
@@ -39,7 +55,7 @@ function lookFor(){
 			      .append("p")
 			      	.attr("id", "igt_count")
 			        .text(function() {
-			            return d.igt_count;
+			            return "Number of igts found: " + d.igt_count;
 			     	});
 			    d3.select("#igt_id")
 			      .selectAll("li")
@@ -50,11 +66,38 @@ function lookFor(){
 			      	.on("click", function(d){
 			      		d3.select("#igt").html("");
 			      		igtLayout("#igt", d);
+			      		igtSelected(this);
 			      	})
 			        .text(function(d) {
 			        	return d.id;
 			        });
+			        
+
+			    // Sets up user feedback for clicked igt
+			    var listLinks = $("#igt_id li");
+			    for (var i = 0; i < listLinks.length; i++) {
+			    	listLinks[i].setAttribute("id", "listitem" + i);
+			    }
+
+
+			    $("#loadingicon").hide();
+			    $("#num").show();
+				$("#igt_id").show();
+
+
     		});
     		return false;
     	// igtLayout("#igt", xigtJsonData);
-	};
+	}
+
+	//aTag is newly selected igt.
+	//aTag is highlighted while previously selected igt
+	// is unhighighted.
+	function igtSelected(aTag) {
+		if (currListItem != null) {
+			$("#" + currListItem).removeClass("selected");
+		}
+		aTag.parentElement.classList.add("selected");
+		currListItem = aTag.parentElement.getAttribute("id");
+	}
+
