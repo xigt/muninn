fa24578b6c8db4ce7eeb668d64fffb79770373c3
Abdullahi Diriye <aidiriye001@gmail.com>
2017-04-02 17:57:49 -0700

    Added initial search results section, two columns
    -Added an intial search results section that displays igt ids and
    additional information depending on what tier the user searched in.
    
    If the user searched the phrases tier than the phrase that is stored in
    the phrase tier for each igt, is displayed next to the igt ids.
    
    If the user searched the word tier, than the words that make up the
    phrases described above are displayed next to each igt id.
    
    If the user searched the morphemes tier than the morphemes that make up
    the words described above are displayed next to the igt ids.
    
    -Once one of the igts is selected in the initial search result section,
    the user is taken to a results section with two columns. On the left
    there is a list of igt ids for found igts. On the right is the data for
    the currently selected igt. The igts on the left can be selected.
    
    -Added a button to allow the user to go back to the intial search
    results section.
    
    -Added styling to the intial search results section and the button that
    allows the user to go back to it.

--- static/search.js
+++ static/search.js
@@ -20,33 +20,21 @@
    
                 .attr("value", function(d){return d.id;});
 
-      
-        });
-        
-	};
-
-
-
-	
-	
+                $("#back").click(goBack);
+        });     
+	};	
 })();
 
 var currListItem = null;
 
 function lookFor(){
-		d3.select("#num").selectAll("p").remove();
-		d3.select("#igt_id").selectAll("li").remove();
-		$("#igt").html("");
-		$("#loadingicon").show();
-
-		$("#num").hide();
-		$("#igt_id").hide();
-		
+		loadingAnimation();
 
 		var name = document.forms.searchForm.corpus.value;
 		var word = document.forms.searchForm.words.value;
 		d3.xhr("http://odin.xigt.org/v1/corpora/" + name + "/igts?match=" + 
-					encodeURIComponent("tier[@type=\""+  $("#tiertype").val()  +"\"]/item[value()=\"" + word + "\"]"))
+					encodeURIComponent("tier[@type=\""+  $("#tiertype").val()  + 
+						"\"]/item[value()=\"" + word + "\"]"))
 			.mimeType("application/json")
 			.response(function(xhr) { return JSON.parse(xhr.responseText); })
 			.get(function(error, d) {
@@ -62,42 +50,215 @@ function lookFor(){
 			      .data(d.igts)
 			      .enter()
 			      .append("li")
-			      	.append("a")
+			      	//.append("a")
 			      	.on("click", function(d){
 			      		d3.select("#igt").html("");
 			      		igtLayout("#igt", d);
 			      		igtSelected(this);
+			      		lengthen();			  
 			      	})
-			        .text(function(d) {
-			        	return d.id;
-			        });
-			        
-
-			    // Sets up user feedback for clicked igt
-			    var listLinks = $("#igt_id li");
-			    for (var i = 0; i < listLinks.length; i++) {
-			    	listLinks[i].setAttribute("id", "listitem" + i);
-			    }
-
-
-			    $("#loadingicon").hide();
-			    $("#num").show();
-				$("#igt_id").show();
-
-
+			      	.text(function(d) {
+			      			debugger;
+				        	return d.id;
+				     });
+				  var tr = d3.select("#sentences")
+			    	.selectAll("td")
+			    	.data(d.igts)
+			      	.enter()
+			      	.append("tr");
+			      	tr.on("click", function(d){
+			      		d3.select("#igt").html("");
+			      		igtLayout("#igt", d);
+			      		$("#sentences").hide();
+			      		$("#igtdisplay").show();
+			      		$("#back").show();
+			      		var id = this.getAttribute("id");
+			      		// change here
+			      		igtSelected(document.getElementById("scroll" + id));
+			      		lengthen();	
+			      		$('html,body').scrollTop(0);		  
+			      	});
+			      	tr.append("td")
+			      		.text(function(d) {
+			      			return d.id;
+			      		})
+			      		.attr("width", "30%");
+			      	if ($("#tiertype").val() == "phrases") {
+				      	tr.append("td")
+				      		.append("span")
+				      		.text(function(d) {
+				      			return getPhrase(d);
+				      		});
+			      	} else if ($("#tiertype").val() == "words") {
+			      		tr.append("td")
+			      			.selectAll("span")
+					    	.data(function(d) {
+					    		return getWords(d);
+					    	})
+					      	.enter()
+					      	.append("span")
+					      	.text(function(d) {
+					      		return d;
+					      	});
+			      	} else if ($("#tiertype").val() == "morphemes") {
+			      		tr.append("td")
+			      			.selectAll("span")
+					    	.data(function(d) {
+					    		return getMorphemes(d);
+					    	})
+					      	.enter()
+					      	.append("span")
+					      	.text(function(d) {
+					      		return d;
+					      	});
+			      	}
+			    loadResults();
+			
     		});
     		return false;
     	// igtLayout("#igt", xigtJsonData);
 	}
 
-	//aTag is newly selected igt.
-	//aTag is highlighted while previously selected igt
-	// is unhighighted.
-	function igtSelected(aTag) {
+	function loadingAnimation() {
+		d3.select("#num").selectAll("p").remove();
+		d3.select("#igt_id").selectAll("li").remove();
+		d3.select("#sentences").selectAll("tr").remove();
+		$("#igt").html("");
+		$("#num").hide();
+		$("#sentences").hide();
+		$("#igtdisplay").hide();
+		$("#back").hide();
+		$("#loadingicon").show();
+	}
+
+	// I changed this from li to tr
+	function loadResults() {
+		var listItems = $("#igt_id li");
+		var listItems2 = $("#sentences tr");
+	    for (var i = 0; i < listItems.length; i++) {
+	    	listItems[i].setAttribute("id", "scrolllistitem" + i);
+	    	listItems2[i].setAttribute("id", "listitem" + i);
+	    }
+	    $("#loadingicon").hide();
+	    $("#num").show();
+	    $("#sentences").show();
+	    if (listItems.length > 0) {
+	    	$("#igtlist").show();
+	    }
+	}
+
+	function igtSelected(liTag) {
 		if (currListItem != null) {
 			$("#" + currListItem).removeClass("selected");
 		}
-		aTag.parentElement.classList.add("selected");
-		currListItem = aTag.parentElement.getAttribute("id");
+		liTag.classList.add("selected");
+		currListItem = liTag.getAttribute("id");
 	}
 
+	function getPhrase(igt) {
+		var tiers = igt.tiers;
+		var phraseTier = findTier(tiers, "phrases");
+		if (phraseTier.items[0].text != undefined) {
+			return igt.id + " " + phraseTier.items[0].text;
+		} else {
+			var itemId = phraseTier.items[0].attributes.content;
+			var tierId = phraseTier.attributes.content;
+			var items = idMatcher(tiers, tierId).items;
+			var item = idMatcher(items, itemId);
+			return /*igt.id + " " + */item.text;
+			
+		}
+
+	}
+
+	function findTier(tiers, type) {
+		var found = false;
+		var position = 0;
+		var foundTier = null;
+		while (!found && position <= tiers.length - 1) {
+			if (tiers[position].type == type) {
+				foundTier = tiers[position];
+				found = true;
+			}
+			position++;
+		}
+		return foundTier;
+	}
+
+	function idMatcher(arr, id) {
+		var matched = false;
+		var index = 0;
+		var found = null;
+		while (!matched && index <= arr.length - 1) {
+			if (arr[index].id == id) {
+				matched = true;
+				found = arr[index];
+			}
+			index++;
+		}
+		return found;
+	}
+
+	function lengthen() {
+		var height = $("#igt").css("height");
+		$("#igtlist").css("max-height", height);
+	}
+
+	function goBack() {
+		$("#back").hide();
+		$("#igtdisplay").hide();
+		$("#sentences").show();
+		$('html,body').scrollTop(0);
+	}
+
+	function getWords(igt) {
+		debugger;
+		var phrase = getPhrase(igt);
+		var tiers = igt.tiers;
+		var wordTier = findTier(tiers, "words");
+		var items = wordTier.items;
+		var words = [];
+		for (var i = 0; i < items.length; i++) {
+			var span = getSpan(items[i].attributes.segmentation);
+			var word = phrase.substring(span[0], span[1]);
+			words.push(word);
+		}
+		return words;
+	}
+
+	function getSpan(segment) {
+		var index1 = segment.indexOf("[");
+		var index2 = segment.indexOf(":");
+		var index3 = segment.indexOf("]");
+		var firstNum = parseInt(segment.substring(index1 + 1, index2));
+		var lastNum = parseInt(segment.substring(index2 + 1, index3));
+		var arr = [firstNum, lastNum];
+		return arr;
+	}
+
+	function getMorphemes(igt) {
+		var words = getWords(igt);
+		var tiers = igt.tiers;
+		var morphemeTier = findTier(tiers, "morphemes");
+		var items = morphemeTier.items;
+		var morphemes = [];
+		for (var i = 0; i < items.length; i++) {
+			var segment = items[i].attributes.segmentation;
+			if (!segment.includes("[")) {
+				wordNumber = parseInt(segment.substring(1));
+				morphemes.push(words[wordNumber - 1]);
+			} else {
+				var index = segment.indexOf("[");
+				var wordNumber = parseInt(segment.substring(1, index));
+				var span = getSpan(segment);
+				morphemes.push(words[wordNumber - 1].substring(span[0], span[1]));
+			}
+		}
+		return morphemes;
+	}
+
+
+
+
+	
+	
